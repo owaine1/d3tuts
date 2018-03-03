fd833abc24ae3c216b95d3f4c64a7eb1e5ec4171
owaine1 <owaine1@live.com>
2018-03-03 09:05:14 +0800

    Most people will listen to your unreasonable demands, if you'll consider their unacceptable offer.

--- 2SimpleBarChart/shapes.js
+++ 2SimpleBarChart/shapes.js
@@ -11,11 +11,25 @@ svg.selectAll("rect")
     .attr("x", function (dataPoint, index) {
         return index * 60;
     })
-    .attr("y", function (dataPoint, index) {
+    .attr("y", function (dataPoint) {
         return 300 - (dataPoint * 15)
     })
     .attr("width", "50")
-    .attr("height", function (dataPoint, index) {
+    .attr("height", function (dataPoint) {
         return dataPoint * 15;
     })
     .attr("fill", "pink");
+
+var newX = 300;
+
+svg.selectAll("circle")
+    .data(dataArray)
+    .enter().append("circle")
+    .attr("cx", function (dataPoint, index) {
+        newX = +(dataPoint * 6) + (index * 20);
+        return newX;
+    })
+    .attr("cy", "100")
+    .attr("r", function (dataPoint) {
+        return dataPoint * 3;
+    });
