54ba9d6920a98cd91be96930927814497042c4a6
owaine1 <owaine1@live.com>
2018-03-06 12:09:32 +0800

    Punning is the worst vice, and there's no vice versa.

--- 5ScalesandAxes/5bcLinearScale.js
+++ 5ScalesandAxes/5bcLinearScale.js
@@ -4,13 +4,20 @@ var dataYears = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007',
 var width = 500;
 var height = 200;
 
+var margin = {
+    left: 50,
+    right: 50,
+    top: 40,
+    bottom: 0
+}
 // structure scales always take
 var y = d3.scaleLinear()
     .domain([0, 180]) //input. Always take an array
     .range([height, 0]); //output. Always take an array
 
-console.log(y(0), '\n', y(90), '\n', y(180)); //test scale
+// console.log(y(0), '\n', y(90), '\n', y(180)); //test scale
 
+var yAxis = d3.axisLeft(y); // axisLeft is a generator
 
 var area = d3.area()
     .x(function (d, i) {
@@ -22,4 +29,11 @@ var area = d3.area()
     });
 
 var svg = d3.select("body").append("svg").attr("width", "100%").attr("height", "100%");
-svg.append("path").attr("d", area(dataArray));
+var chartGroup = svg.append("g").attr("transform",
+    "translate(" + margin.left + ", " + margin.top + ")");
+
+chartGroup.append("path").attr("d", area(dataArray));
+
+chartGroup.append("g")
+    .attr("class", "axis y")
+    .call(yAxis);
