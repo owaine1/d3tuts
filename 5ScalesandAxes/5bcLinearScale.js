d38e0ee16e364a363b1bc6d3df0c442113128264
owaine1 <owaine1@live.com>
2018-03-06 15:31:51 +0800

    Now my EMOTIONAL RESOURCES are heavily committed to 23% of the SMELTING and REFINING industry of the state of NEVADA!!

--- 5ScalesandAxes/5bcLinearScale.js
+++ 5ScalesandAxes/5bcLinearScale.js
@@ -1,6 +1,16 @@
 var dataArray = [25, 26, 28, 32, 37, 45, 55, 70, 90, 120, 135, 150, 160, 168, 172, 177, 180];
 var dataYears = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'];
 
+var parseDate = d3.timeParse("%Y");
+// console.log(parseDate('2003'), '2003');
+// console.log(d3.max(dataYears, function (d) {
+//     return parseDate(d);
+// }));
+// console.log(d3.extent(dataYears, function (d) {
+//     return parseDate(d);
+// }));
+
+
 var width = 500;
 var height = 200;
 
@@ -11,17 +21,30 @@ var margin = {
     bottom: 0
 }
 // structure scales always take
+var x = d3.scaleTime()
+    .domain(d3.extent(dataYears, function (d) {
+        return parseDate(d);
+    }))
+    .range([0, width]);
+
+// console.log(x(parseDate('2010')));
+
 var y = d3.scaleLinear()
-    .domain([0, 180]) //input. Always take an array
+    .domain([0, d3.max(dataArray)]) //input. Always take an array
     .range([height, 0]); //output. Always take an array
 
 // console.log(y(0), '\n', y(90), '\n', y(180)); //test scale
 
-var yAxis = d3.axisLeft(y); // axisLeft is a generator
+var xAxis = d3.axisBottom(x);
+// axisLeft is a generator/ ticks on LHS
+var yAxis = d3.axisLeft(y)
+    .ticks(3)
+    .tickPadding(10)
+    .tickSize(10);
 
 var area = d3.area()
     .x(function (d, i) {
-        return i * 20;
+        return x(parseDate(dataYears[i]));
     })
     .y0(height)
     .y1(function (d) {
@@ -37,3 +60,8 @@ chartGroup.append("path").attr("d", area(dataArray));
 chartGroup.append("g")
     .attr("class", "axis y")
     .call(yAxis);
+
+chartGroup.append("g")
+    .attr("class", "axis x")
+    .attr("transform", "translate(0, " + height + ")")
+    .call(xAxis);
